3adabf04624c67ae3b005e19cc348fe6e6b45968
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-12-08 22:16:32 +0800

    init day 2

---
+++ 2025/day2/part1/main.cpp
@@ -0,0 +1,3 @@
+// C++ solution for Day 2 Part 1
+#include <iostream>
+int main() { /* Part 1 code */ return 0; }
---
+++ 2025/day2/part2/main.cpp
@@ -0,0 +1,3 @@
+// C++ solution for Day 2 Part 2
+#include <iostream>
+int main() { /* Part 2 code */ return 0; }
